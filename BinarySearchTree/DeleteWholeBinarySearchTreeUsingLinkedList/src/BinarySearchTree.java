c383b59c93d0a3238acaf02e898c087be9c1785d
Om Bhardwaj <omb6464@gmail.com>
2023-06-10 03:49:59 +0530

    Deleted Full Binary Search Tree

---
+++ BinarySearchTree/DeleteWholeBinarySearchTreeUsingLinkedList/src/BinarySearchTree.java
@@ -0,0 +1,36 @@
+public class BinarySearchTree {
+    BinaryNode root;
+
+    public BinarySearchTree() {
+        root = null;
+        System.out.println("BST is created successfully");
+    }
+    
+    private BinaryNode insert(BinaryNode currentNode,int value) {
+        if (currentNode == null) {
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = value;
+            System.out.println("The Node is inserted successfully");
+            return newNode;
+        } else if (value <= currentNode.value) {
+            currentNode.left = insert(currentNode.left , value);
+            return currentNode;
+        } else {
+            currentNode.right = insert(currentNode.right , value);
+            return currentNode;
+        }
+    }
+
+    public void insert(int value) {
+        root = insert(root,value);
+    }
+
+    public void deleteBST() {
+        root = null;
+        System.out.println("Binary Search Tree Has Been deleted successfully");
+    }
+
+
+}
+
+
