6ef76b866a6903a05b6fcff904b9efc10d305c81
Om Bhardwaj <omb6464@gmail.com>
2023-06-10 17:20:13 +0530

    Completed Creation Of BinaryHeap

---
+++ BinaryHeap/src/BinaryHeap.java
@@ -0,0 +1,10 @@
+public class BinaryHeap {
+    int arr[];
+    int sizeOfTree;
+
+    public BinaryHeap(int size) {
+        this.arr = new int[size+1];
+        this.sizeOfTree = 0;
+        System.out.println("Binary heap is Created");
+    }
+}
---
+++ BinaryHeap/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryHeap bH = new BinaryHeap(5);
+    }
+}
