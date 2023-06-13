7335b8002d95b0adf998cfc92b2098c5a8c835ad
Om Bhardwaj <omb6464@gmail.com>
2023-06-14 00:51:00 +0530

    Implemented Bubble sort

---
+++ SortingAlgorithms/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int[] arr = {10, 5, 15, 6, 17};
+      BubbleSort bS = new BubbleSort();
+      bS.bubbleSort(arr);
+      bS.printArray(arr);
+    }
+}
