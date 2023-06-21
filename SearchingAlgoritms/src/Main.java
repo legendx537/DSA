988706da090ed1c3cf48406f003a9fed00174460
Om Bhardwaj <omb6464@gmail.com>
2023-06-21 15:40:52 +0530

    Completed Binary Search implementation

--- SearchingAlgoritms/src/Main.java
+++ SearchingAlgoritms/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
       int[] arr = {1, 2, 5, 10, 4, 6};
+      System.out.println("Linear Search :- ");
       Searching.linearSearch(arr,10);
       Searching.linearSearch(arr,20);
+
+      System.out.println("Binary Search :- ");
+      int[] arr1 = {2, 3, 4, 5, 6, 8, 10};
+      Searching.binarySearch(arr1,8);
     }
 }
--- SearchingAlgoritms/src/Searching.java
+++ SearchingAlgoritms/src/Searching.java
@@ -10,4 +10,23 @@ public class Searching {
         System.out.println("The value " + value + " is not found in the array");
         return -1;
     }
+
+    public static int binarySearch(int[] arr,int value) {
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left <= right) {
+            int middle = left + (right-left)/2;
+            if (arr[middle] == value) {
+                System.out.println("The value is located at index "+ middle);
+                return middle;
+            } else if (arr[middle] > value) {
+                right = middle-1;
+            } else {
+                left = middle+1;
+            }
+        }
+        System.out.println("The value" + value + " is not found in the array");
+        return -1;
+    }
 }
