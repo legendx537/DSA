b56db428ff3e431df0d854fbd308d1aaf8f7efa1
Om Bhardwaj <omb6464@gmail.com>
2023-06-22 16:31:10 +0530

    Revised Fibonaaci Number

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         Main recursion = new Main();
         int factorial = recursion.factorial(4);
         System.out.println(factorial);
+
+        System.out.println("Fibonacci Implementation :- ");
+        int fibonacci = recursion.fibonacci(6);
+        System.out.println(fibonacci);
     }
     public int factorial(int n) {
         if (n<0) {
@@ -12,5 +16,15 @@ public class Main {
             return 1;
         }
         return n*factorial(n-1);
+    }
+
+    public int fibonacci(int n) {
+        if (n<0) {
+            return -1;
+        }
+        if (n==0 || n==1) {
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 }
